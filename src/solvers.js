85a41d442fec9f609604253d3c7fa63f88b54ef5
Hack Reactor Student <student@hackreactor.com>
2017-07-05 19:44:22 -0500

    Started on solver but was stuck badly

--- src/solvers.js
+++ src/solvers.js
@@ -14,12 +14,53 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 
-
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+window.findNRooksSolution = function(n
+  ) {
+  
+  //var solution = {};
+  var myBoard = new Board({"n": n});
+  var arrayOfCoordsToggled = [];
+  var totalPiece = n;
+  //myBoard.togglePiece(0, 0);
+  function isOff(xVal, yVal) {
+    return myBoard.attribute[xVal][yVal] === 0;
+  }
+  
+  function recursion(myBoard) {
+    // if (arrayOfCoordsToggled.length === n); {
+      
+    // }
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        if (isOff(i, j)) {
+          myBoard.togglePiece(i, j);
+          arrayOfCoordsToggled.push([i, j]);
+          if (!myBoard.hasAnyRooksConflicts()) {
+            recursion(myBoard);
+          }
+         
+        } 
+      }
+    }
+      
+    
+  }
+  recursion(myBoard);
+  // for ()
+  // solution[xVal] = yVal;
+  // solution.n = n;
+  // return solution;
+    // togglePeice(0, i);
+    // if (hasAnyRooksConflicts()) {
+    //   // we go check down one row
+    // }
+    // if (!hasAnyRooksConflicts) {
+    //   togglePiece();
+    // }
+  console.log(arrayOfCoordsToggled)
+  //myBoard.togglePiece();
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(myBoard));
+  return myBoard;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
@@ -32,10 +73,10 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var myBoard = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  console.log('Single myBoard for ' + n + ' queens:', JSON.stringify(myBoard));
+  return myBoard;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
